387ffdb62b78ec09f3fce521ca2d3a2995fcc2d9
Viktor Koncz <konczviktor@gmail.com>
2021-06-12 14:26:42 +0200

    Add react-native-maps package

--- src/pages/MapTab/MapTab.tsx
+++ src/pages/MapTab/MapTab.tsx
@@ -1,6 +1,19 @@
 import React, { ReactElement } from 'react';
-import { Text } from 'react-native';
+import MapView from 'react-native-maps';
+
+import s from './MapTab.style';
 
 export function MapTab(): ReactElement {
-    return <Text>Map tab</Text>;
+    return (
+        <MapView
+            provider="google"
+            style={s.map}
+            region={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.015,
+                longitudeDelta: 0.0121,
+            }}
+        />
+    );
 }
